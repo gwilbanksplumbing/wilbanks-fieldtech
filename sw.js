f29ea8f32a5d0cf8dd567ee4b48a3c2432a3ecec
Wilbanks <gwillma@gmail.com>
2026-05-10 17:41:20 +0000

    Fix tab bar cold launch: add id=wc-nav to nav element, CSS position:fixed override + GPU layer hint, SW v24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v23";
+const CACHE = "wc-fieldtech-v24";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
