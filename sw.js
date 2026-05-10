ac850431971c741ac9999001abacab0fb48900b4
Wilbanks <gwillma@gmail.com>
2026-05-10 18:35:20 +0000

    Fix tab bar gap: body min-height:-webkit-fill-available so flex column fills true iOS screen height, SW v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v34";
+const CACHE = "wc-fieldtech-v35";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
