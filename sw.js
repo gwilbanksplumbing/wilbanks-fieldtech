6de42d669b86026a452357251731bca91129c523
Wilbanks <gwillma@gmail.com>
2026-05-10 17:48:05 +0000

    Fix tab bar cold launch: convert nav to flex child (remove position:fixed), move nav after content div, SW v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v24";
+const CACHE = "wc-fieldtech-v25";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
