3a8d0beddbf476b1e850e08be8933cdfda524bb3
Wilbanks <gwillma@gmail.com>
2026-05-11 22:40:02 +0000

    Fix: add auth header to photo upload and single appointment fetch, SW v64

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v63";
+const CACHE = "wc-fieldtech-v64";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
