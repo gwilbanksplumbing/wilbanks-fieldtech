053991ff9bef5a9403d5048f18461796d9da289f
Wilbanks <gwillma@gmail.com>
2026-05-11 01:26:56 +0000

    Equipment: simplify to unified editable list + add location field (dropdown + card display), SW v56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v55";
+const CACHE = "wc-fieldtech-v56";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
