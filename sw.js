cd3cdabc03163064563c2acd7ccb4b6ad4862875
Wilbanks <gwillma@gmail.com>
2026-05-11 00:31:25 +0000

    Equipment: hide Edit/Delete in detail view for prior-visit items, SW v54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v53";
+const CACHE = "wc-fieldtech-v54";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
