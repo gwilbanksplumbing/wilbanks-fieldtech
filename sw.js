4cc7fdbff26be8e21f315438762c6a95dc827eb0
Wilbanks <gwillma@gmail.com>
2026-05-12 02:02:20 +0000

    Push: skip registration for admin user, SW v67, cache bust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v66";
+const CACHE = "wc-fieldtech-v67";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
