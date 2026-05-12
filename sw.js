b9adf5e0047b398ada8fd27ea7b68edbd9eb875c
Wilbanks <gwillma@gmail.com>
2026-05-12 01:57:32 +0000

    Push dedup: unsubscribe existing endpoint before re-registering, SW v66, cache bust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v65";
+const CACHE = "wc-fieldtech-v66";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
