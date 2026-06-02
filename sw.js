0a42fef251ec5110a6ac8f46939d8ae9b4e0ac16
Glenn Wilbanks <gwillma@gmail.com>
2026-06-02 03:16:36 +0000

    PROMOTE field app staging->prod: job-list scroll restore (back/browser-back/full-refresh lands on tapped job; cold open=top) + selected secondary contacts display on job detail; bundle index-C1VS8TqU.js->index-Ckdo3J-i.js, css index-Dinbi755.css->index-DysosUmp.css; SW v72->v73; stamps bumped

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v72";
+const CACHE = "wc-fieldtech-v73";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
