667c1f148dc0b4db68733693fe9e9ed687a78a08
Glenn Wilbanks <gwillma@gmail.com>
2026-06-30 15:57:44 +0000

    PROMOTE field app staging->prod: Apple Maps Directions (daddr+dirflg=d) + tappable address (?q=); bundle index-Dcalllog02.js -> index-Dapplemap01.js; SW v78 -> v79-applemap; ?v bump (built from src 549725c, revert tag prod-pre-applemap-20260630-103730 @ c52f3cd)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-fieldtech-v78";
+const CACHE = "wc-fieldtech-v79-applemap";
 const API_BASE = "https://wilbanks-server-production.up.railway.app";
 
 self.addEventListener("install", () => {
